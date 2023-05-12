b9315539ccfd151e6733914ce72609fc8efddc84
Rajnesh Kumar <109650270+rajyadav0001@users.noreply.github.com>
2023-05-12 20:51:29 +0530

    Router setup done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
  
 import Header from "./Components/Header";
 import Cards from "./Components/Cards";
+import { Route, Routes } from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
         <Header/>
-        <Cards/>
+        <Routes>
+            <Route path="/" element={<Cards/>} />
+        </Routes>
     </div>
   );
 }
